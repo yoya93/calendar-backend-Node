9596210131acd1c44465646d20e065ab6bbad3dc
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-13 00:34:49 -0300

    Added write in Mongdb

--- contollers/auth.js
+++ contollers/auth.js
@@ -1,15 +1,26 @@
 const expess = require("express");
+const Usuario = require("../models/Usuario");
 
-const registerUser = (req, res = expess.response) => {
-  const { name, email, password } = req.body;
+const registerUser = async (req, res = expess.response) => {
+  // const { name, email, password } = req.body;
 
-  res.status(200).json({
-    ok: true,
-    msg: "register",
-    name,
-    email,
-    password,
-  });
+  try {
+    const usuario = new Usuario(req.body);
+
+    await usuario.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: "register",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
 };
 
 const loginUser = (req, res = expess.response) => {
