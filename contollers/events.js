4bc6dae91e1bf2aa6d5987c9b22f286253044e9a
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-13 18:33:13 -0300

    Create event in mongoDb

--- contollers/events.js
+++ contollers/events.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Evento = require("../models/Evento");
 
 // Obtener eventos
 const getEvents = (req, res = express.response) => {
@@ -9,7 +10,27 @@ const getEvents = (req, res = express.response) => {
 };
 
 // Crear un nuevo evento
-const createEvent = (req, res = express.response) => {
+const createEvent = async (req, res = express.response) => {
+  const evento = new Evento(req.body);
+
+  try {
+    evento.user = req.uid;
+
+    const eventGuardado = await evento.save();
+
+    return res.status(200).json({
+      ok: true,
+      event: eventGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: "Por favor hable con el administrador",
+    });
+  }
+
   res.status(200).json({
     ok: true,
     msg: "createEvent",
