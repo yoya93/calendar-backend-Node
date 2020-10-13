277df50068101a29747bddbc3b97e13cbba9770d
jorge <jorgelopezgonzalez474@gmail.com>
2020-10-13 17:10:07 -0300

    Added models events

--- contollers/events.js
+++ contollers/events.js
@@ -8,6 +8,8 @@ const getEvents = (req, res = express.response) => {
 };
 
 const createEvent = (req, res = express.response) => {
+  console.log(req.body);
+
   res.status(200).json({
     ok: true,
     msg: "createEvent",
